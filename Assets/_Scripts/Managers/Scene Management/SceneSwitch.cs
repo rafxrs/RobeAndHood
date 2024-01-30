30b373f0e031c5dd4bdab7d72ffe515f372177fb
Rafael Reis <rafael.reis@mail.mcgill.ca>
2024-01-30 10:22:56 -0500

    remove warnings in SceneSwitch.cs and World.sc

--- Assets/_Scripts/Managers/Scene Management/SceneSwitch.cs
+++ Assets/_Scripts/Managers/Scene Management/SceneSwitch.cs
@@ -5,18 +5,18 @@ namespace _Scripts.Managers.Scene_Management
 {
     public class SceneSwitch : MonoBehaviour
     {
-        public static string currentScene;
-        public static string previousScene;
+        private static string _currentScene;
+        protected static string previousScene;
         // Start is called before the first frame update
         public virtual void Start()
         {
-            currentScene = SceneManager.GetActiveScene().name;
+            _currentScene = SceneManager.GetActiveScene().name;
         }
 
         // Method to load new scene
         public void SwitchScene(string sceneName)
         {
-            previousScene = currentScene;
+            previousScene = _currentScene;
             SceneManager.LoadScene(sceneName);
         } 
     }
