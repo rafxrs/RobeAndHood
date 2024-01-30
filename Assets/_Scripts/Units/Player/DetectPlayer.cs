424a16d6386b77525c5e04b7721b0dd6f8083510
Rafael Reis <rafael.reis@mail.mcgill.ca>
2024-01-30 10:20:34 -0500

    remove warnings in DetectPlayer.cs

--- Assets/_Scripts/Units/Player/DetectPlayer.cs
+++ Assets/_Scripts/Units/Player/DetectPlayer.cs
@@ -1,191 +1,188 @@
-using System.Collections;
-using System.Collections.Generic;
 using _Scripts.Managers;
-using _Scripts.Units.Player;
 using UnityEngine;
 
-public class DetectPlayer : MonoBehaviour
+namespace _Scripts.Units.Player
 {
-    public bool playerDetected;
-    // public Transform areaPos;
-    // public float areaWidth;
-    // public float areaHeight;
-    // public LayerMask whatIsPlayer;
-    public Action action;
-    public GameObject doActionOn;
-    GameObject player;
-    Transform playerTransform;
-    GameManager gameManager;
-
-    
-    // Start is called before the first frame update
-    void Start()
+    public class DetectPlayer : MonoBehaviour
     {
-        player = GameObject.Find("Player");
-        playerTransform = player.GetComponent<Transform>();
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-    }
+        public bool playerDetected;
+        // public Transform areaPos;
+        // public float areaWidth;
+        // public float areaHeight;
+        // public LayerMask whatIsPlayer;
+        public Action action;
+        public GameObject doActionOn;
+        GameObject _player;
+        Transform _playerTransform;
+        GameManager _gameManager;
+        private static readonly int Open = Animator.StringToHash("Open");
 
-    // Update is called once per frame
-    void Update()
-    {
-        // playerDetected = Physics2D.OverlapBox(areaPos.position, new Vector2(areaWidth, areaHeight),0,whatIsPlayer);
 
-        if (playerDetected)
+        // Start is called before the first frame update
+        void Start()
         {
-            // do action
-            switch (action)
+            _player = GameObject.Find("Player");
+            _playerTransform = _player.GetComponent<Transform>();
+            _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // playerDetected = Physics2D.OverlapBox(areaPos.position, new Vector2(areaWidth, areaHeight),0,whatIsPlayer);
+
+            if (playerDetected)
             {
-                case Action.Dialogue:
-                    gameManager.ShowEButton();
-                    if (Input.GetKeyDown(KeyCode.E))
+                // do action
+                switch (action)
+                {
+                    case Action.Dialogue:
+                        _gameManager.ShowEButton();
+                        if (Input.GetKeyDown(KeyCode.E))
                         {
                             GetComponent<DialogueTrigger>().TriggerDialogue();
-                            gameManager.HideEButton();
+                            _gameManager.HideEButton();
                         }
-                    break;
-                case Action.Door:
-                    gameManager.ShowEButton();
-                    if (Input.GetKeyDown(KeyCode.E))
-                    {
-                        Debug.Log("Opening the door");
-                        playerTransform.position = doActionOn.transform.position;
-                        gameManager.HideEButton();
-                    }
-                    break;
-                case Action.Tutorial:
-                    GetComponent<DialogueTrigger>().TriggerDialogue();
-                    break;
-                case Action.SelectWeapon:
-                    gameManager.ShowEButton();
-                    // highlight specific weapon
-                    doActionOn.SetActive(true);
-                    if (Input.GetKeyDown(KeyCode.E))
-                    {
-                        int num=0;
-                        switch (this.name)
+                        break;
+                    case Action.Door:
+                        _gameManager.ShowEButton();
+                        if (Input.GetKeyDown(KeyCode.E))
                         {
-                            case "Spear":
-                                num = 0;
-                                break;
-                            case "Bow":
-                                num = 1;
-                                break;
-                            case "Sword":
-                                num = 2;
-                                break;
-                            default:
-                                break;
+                            Debug.Log("Opening the door");
+                            _playerTransform.position = doActionOn.transform.position;
+                            _gameManager.HideEButton();
                         }
-                        player.GetComponent<PlayerCombat>().ChooseWeapon(num);
-                        FindObjectOfType<AudioManager>().Play("Equip");
-                        // GameObject ws = GameObject.Find("WeaponSelect");
-                        // Destroy(ws);
-                        Debug.Log("Selecting Weapon");
-                        // doActionOn.SetActive(false);
-                        gameManager.HideEButton();
-                    }
-                    break;
-                case Action.EndOfLevel:
-                    gameManager.ShowEButton();
-                    if (Input.GetKeyDown(KeyCode.E))
-                    {
-                        Debug.Log("Finishing level");
-                        gameManager.LevelComplete(); 
-                        Destroy(this.gameObject);
-                    }
-                    break;
-                case Action.Chest:
-                    gameManager.ShowEButton();
-                    // if key, unlock chest;
-                    // if no key, show missing key
-                    if (Input.GetKeyDown(KeyCode.E))
-                    {
-                        bool unlocked = player.GetComponent<Player>().UnlockChest();
-                        if (unlocked)
+                        break;
+                    case Action.Tutorial:
+                        GetComponent<DialogueTrigger>().TriggerDialogue();
+                        break;
+                    case Action.SelectWeapon:
+                        _gameManager.ShowEButton();
+                        // highlight specific weapon
+                        doActionOn.SetActive(true);
+                        if (Input.GetKeyDown(KeyCode.E))
                         {
-                            Animator animator = GetComponent<Animator>();
-                            if (animator !=null)
+                            int num=0;
+                            switch (this.name)
                             {
-                                animator.SetTrigger("Open");
+                                case "Spear":
+                                    num = 0;
+                                    break;
+                                case "Bow":
+                                    num = 1;
+                                    break;
+                                case "Sword":
+                                    num = 2;
+                                    break;
                             }
-                            GetComponent<RewardSpawner>().Reward(transform.position);
-                            gameManager.HideEButton();
-                            this.enabled = false;;
+                            _player.GetComponent<PlayerCombat>().ChooseWeapon(num);
+                            FindObjectOfType<AudioManager>().Play("Equip");
+                            // GameObject ws = GameObject.Find("WeaponSelect");
+                            // Destroy(ws);
+                            Debug.Log("Selecting Weapon");
+                            // doActionOn.SetActive(false);
+                            _gameManager.HideEButton();
                         }
-                        else 
+                        break;
+                    case Action.EndOfLevel:
+                        _gameManager.ShowEButton();
+                        if (Input.GetKeyDown(KeyCode.E))
                         {
-                            player.transform.Find("MissingKey").gameObject.SetActive(true);
-                            Invoke("HideMissingKey", 1f);
+                            Debug.Log("Finishing level");
+                            _gameManager.LevelComplete(); 
+                            Destroy(this.gameObject);
                         }
+                        break;
+                    case Action.Chest:
+                        _gameManager.ShowEButton();
+                        // if key, unlock chest;
+                        // if no key, show missing key
+                        if (Input.GetKeyDown(KeyCode.E))
+                        {
+                            bool unlocked = _player.GetComponent<Player>().UnlockChest();
+                            if (unlocked)
+                            {
+                                Animator animator = GetComponent<Animator>();
+                                if (animator !=null)
+                                {
+                                    animator.SetTrigger(Open);
+                                }
+                                GetComponent<RewardSpawner>().Reward(transform.position);
+                                _gameManager.HideEButton();
+                                this.enabled = false;
+                            }
+                            else 
+                            {
+                                _player.transform.Find("MissingKey").gameObject.SetActive(true);
+                                Invoke(nameof(HideMissingKey), 1f);
+                            }
                         
-                        gameManager.HideEButton();
-                    }
-                    break;
-                case Action.Lever:
-                    gameManager.ShowEButton();
-                    if (Input.GetKeyDown(KeyCode.E))
-                    {
-                        Debug.Log("Switching lever");
-                        GetComponent<Lever>().SwitchLeverState();
-                    }
-                    break;
-                case Action.Teleport:
-                    Debug.Log("Teleporting the player");
-                    playerTransform.position = doActionOn.transform.position;
-                    gameManager.HideEButton();
-                    break;
-                default:
-                    break;
-            }
+                            _gameManager.HideEButton();
+                        }
+                        break;
+                    case Action.Lever:
+                        _gameManager.ShowEButton();
+                        if (Input.GetKeyDown(KeyCode.E))
+                        {
+                            Debug.Log("Switching lever");
+                            GetComponent<Lever>().SwitchLeverState();
+                        }
+                        break;
+                    case Action.Teleport:
+                        Debug.Log("Teleporting the player");
+                        _playerTransform.position = doActionOn.transform.position;
+                        _gameManager.HideEButton();
+                        break;
+                }
             
-        }
-    }
+            }
+        }   
     
-    [System.Serializable]
-    public enum Action
-    {
-        Dialogue,
-        Door,
-        Tutorial,
-        SelectWeapon,
-        EndOfLevel,
-        Chest,
-        Lever,
-        Teleport,
-
-    }
-
-    void OnTriggerStay2D(Collider2D other)
-    {
-        if (other.tag == "Player")
+        [System.Serializable]
+        public enum Action
         {
-            playerDetected = true;
+            Dialogue,
+            Door,
+            Tutorial,
+            SelectWeapon,
+            EndOfLevel,
+            Chest,
+            Lever,
+            Teleport,
+
         }
-    }
-    void OnTriggerExit2D(Collider2D other)
-    {
-        
-        if (other.tag == "Player")
-        {
-            Debug.Log("Player left the collider");
-            playerDetected = false;
-            gameManager.HideEButton();
 
-            if (action == Action.SelectWeapon)
+        void OnTriggerStay2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
             {
-                doActionOn.SetActive(false);
+                playerDetected = true;
             }
         }
-    }
+        void OnTriggerExit2D(Collider2D other)
+        {
+        
+            if (other.CompareTag("Player"))
+            {
+                Debug.Log("Player left the collider");
+                playerDetected = false;
+                _gameManager.HideEButton();
 
-    void HideMissingKey()
-    {
-        player.transform.Find("MissingKey").gameObject.SetActive(false);
+                if (action == Action.SelectWeapon)
+                {
+                    doActionOn.SetActive(false);
+                }
+            }
+        }
+
+        void HideMissingKey()
+        {
+            _player.transform.Find("MissingKey").gameObject.SetActive(false);
+        }
+        // void OnDrawGizmosSelected()
+        // {
+        //     Gizmos.color = Color.blue;
+        //     Gizmos.DrawWireCube(areaPos.position, new Vector3(areaWidth,areaHeight,1));
+        // }
     }
-    // void OnDrawGizmosSelected()
-    // {
-    //     Gizmos.color = Color.blue;
-    //     Gizmos.DrawWireCube(areaPos.position, new Vector3(areaWidth,areaHeight,1));
-    // }
 }
