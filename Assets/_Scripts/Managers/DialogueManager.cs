b625ddd3ed63aa2f29377e94e06cccb43a6db279
Rafael Reis <rafael.reis@mail.mcgill.ca>
2024-01-30 10:42:05 -0500

    fix warnings in MainMenu.cs

--- Assets/_Scripts/Managers/DialogueManager.cs
+++ Assets/_Scripts/Managers/DialogueManager.cs
@@ -57,6 +57,7 @@ namespace _Scripts.Managers
             {
                 EndDialogue();
             }
+            
             else 
             {
                 if (_sentences != null)
--- Assets/_Scripts/Managers/MainMenu.cs
+++ Assets/_Scripts/Managers/MainMenu.cs
@@ -1,75 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
+namespace _Scripts.Managers
 {
-    public bool isMuted = false;
-    // public Text volumeText;
+    public class MainMenu : MonoBehaviour
+    {
+        public bool isMuted;
+        // public Text volumeText;
 
-    void Start()
-    {
-        // volumeText.text = PlayerPrefs.GetString("Sound", "SOUND ON");
-        // int muted = PlayerPrefs.GetInt("Muted", 0);
-        // if (muted==0) {
-        //     isMuted=false;
-        // } else {
-        //     isMuted = true;
-        // }
-
-    }
-    void Update()
-    {
-        // if (Input.GetKeyDown(KeyCode.Escape)) 
+        public void LoadLevel(int level)
+        {
+            SceneManager.LoadScene(level); 
+        }
+        public void LoadMenu()
+        {
+            SceneManager.LoadScene(0); 
+        }
+        // public void LoadSettings()
         // {
-        //     Application.Quit();
-        //     // SceneManager.LoadScene(0  );
+        //     SceneManager.LoadScene(); 
         // }
-    }
-    public void LoadLevel(int level)
-    {
-        SceneManager.LoadScene(level); 
-    }
-    public void LoadMenu()
-    {
-        SceneManager.LoadScene(0); 
-    }
-    // public void LoadSettings()
-    // {
-    //     SceneManager.LoadScene(); 
-    // }
-    // public void LoadCredits()
-    // {
-    //     SceneManager.LoadScene();
-    // }
-    public void MuteAllSound()
-    {
-        AudioListener.volume = 0;
-        
-    }
-    public void UnMuteAllSound()
-    {
-        AudioListener.volume = 1;
-        
-    }
-    public void ChangeVolume()
-    {
-        // if (isMuted)
+        // public void LoadCredits()
         // {
-        //     volumeText.text = "SOUND ON";
-        //     UnMuteAllSound();
-        //     PlayerPrefs.SetString("Sound", "SOUND ON"); 
-        //     PlayerPrefs.SetInt("Muted", 0);
-        //     isMuted = false;
+        //     SceneManager.LoadScene();
+        // }
+        public void MuteAllSound()
+        {
+            AudioListener.volume = 0;
+        
+        }
+        public void UnMuteAllSound()
+        {
+            AudioListener.volume = 1;
+        
+        }
+        public void ChangeVolume()
+        {
+            // if (isMuted)
+            // {
+            //     volumeText.text = "SOUND ON";
+            //     UnMuteAllSound();
+            //     PlayerPrefs.SetString("Sound", "SOUND ON"); 
+            //     PlayerPrefs.SetInt("Muted", 0);
+            //     isMuted = false;
             
-        // } else {
-        //     volumeText.text = "SOUND OFF";
-        //     MuteAllSound();
-        //     PlayerPrefs.SetString("Sound", "SOUND OFF"); 
-        //     PlayerPrefs.SetInt("Muted", 1);
-        //     isMuted = true;
-        // }
+            // } else {
+            //     volumeText.text = "SOUND OFF";
+            //     MuteAllSound();
+            //     PlayerPrefs.SetString("Sound", "SOUND OFF"); 
+            //     PlayerPrefs.SetInt("Muted", 1);
+            //     isMuted = true;
+            // }
+        }
     }
 }
