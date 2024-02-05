a9b70d529165a67e99087e088f58970753c2d432
Rafael Reis <rafael.reis@mail.mcgill.ca>
2024-02-05 16:53:42 -0500

    added smasher obstacle

--- Assets/_Scripts/Units/Player/Player.cs
+++ Assets/_Scripts/Units/Player/Player.cs
@@ -257,6 +257,11 @@ namespace _Scripts.Units.Player
                 //Debug.Log("We can climb");
                 isClimbing = true;
             }
+            if (other.gameObject.CompareTag("DeathFloor"))
+            {
+            
+                TakeDamage(_playerScriptable.baseStats.maxHealth);
+            }
         }
         void OnTriggerExit2D(Collider2D other)
         {
